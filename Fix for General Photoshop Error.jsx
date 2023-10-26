8c9dd4b635e658a50803e029a99a05004be988ff
MarshySwamp <stephenamarsh@gmail.com>
2023-10-26 19:46:54 +1100

    Create Fix for General Photoshop Error.jsx
    Update for an error reported in later versions of Photoshop

---
+++ Fix for General Photoshop Error.jsx
@@ -0,0 +1,11 @@
+// If you receive the following error in later versions of Photoshop:
+
+// Error: General Photoshop error occurred. This functionality may not be available in this version of Photoshop. - <no additional information available>: on line 578
+
+// Then change line 327 from:
+
+var templateName =  decodeURI(templateFile).replace(/\.[^\.]+$/, '');	// strip the extension off
+
+// To:
+
+var templateName = templateFile.replace(/^.+\//, '').replace(/\.[^\.]+$/, '');	// remove the path and strip the extension off
